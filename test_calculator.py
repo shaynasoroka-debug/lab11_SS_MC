b572575f538cfd5f8dd4501fb4b7deabfa1861a4
mayacrecos <mayacrecos@gmail.com>
2025-11-12 10:44:47 -0500

    partner 2 completed

--- test_calculator.py
+++ test_calculator.py
@@ -3,12 +3,16 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, -4), -5)
+        self.assertEqual(add(7, -2), 5)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+
+    def test_subtract(self): # 3 assertions
+        self.assertEqual(sub(10, 4), 6)
+        self.assertEqual(sub(3, 5), -2)
+        self.assertEqual(sub(7, 0), 7)
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -19,19 +23,18 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self): # 1 assertion
+        self.assertRaises(div(0, 5))
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self): # 3 assertions
+        self.assertAlmostEqual(log(8, 2), 3.0)
+        self.assertAlmostEqual(log(100, 10), 2.0)
+        self.assertAlmostEqual(log(math.e, math.e), 1.0)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+
+    def test_log_invalid_base(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            log(10, 1)
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
